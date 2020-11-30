8b02354021a1877e5f71357f6857991ec46b1ceb
ddb324 <jiangdaoran@163.com>
2020-12-01 00:40:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Ddb></Ddb>
+</template>
+
+<script>
+import Ddb from './components/Ddb.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Ddb
+  }
+}
+</script>
---
+++ src/components/Ddb.vue
@@ -0,0 +1,7 @@
+<template>
+<div>我是第一个组件</div>
+</template>
+
+<script>
+
+</script>
