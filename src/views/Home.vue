4252c5ecb32c8b4e951ef94783e3f3875223e21c
ddb324 <jiangdaoran@163.com>
2020-12-01 15:08:21 +0800

    .topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="content">
       <aside>侧边栏</aside>
@@ -10,3 +13,23 @@
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.topnav{
+  background: pink;
+  display: flex;
+  padding:16px;
+  .logo{
+    max-width: 6em;
+    margin-right:auto;
+  }
+  .menu{
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    li{
+      margin:0 1em;
+    }
+  }
+}
+</style>
