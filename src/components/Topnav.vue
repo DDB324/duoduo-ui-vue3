8d5af09f782ed022a374d0a2c0c432c03e12a209
ddb324 <jiangdaoran@163.com>
2020-12-02 22:48:30 +0800

    实现aside切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo" @click="toggleAside">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -9,7 +9,17 @@
 </template>
 
 <script lang="ts">
-export default {};
+import {inject, Ref} from 'vue';
+
+export default {
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
+    const toggleAside = ()=>{
+      asideVisible.value = !asideVisible.value
+    }
+    return {toggleAside}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -18,7 +28,7 @@ export default {};
   display: flex;
   padding: 16px;
   position: relative;
-  z-index:10;
+  z-index: 10;
 
   .logo {
     max-width: 6em;
