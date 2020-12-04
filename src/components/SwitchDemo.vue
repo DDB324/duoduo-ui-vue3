600b5434a4f474b2e4101890ae6c475c5d740daa
ddb324 <jiangdaoran@163.com>
2020-12-04 20:45:00 +0800

    可以接收value和input事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="switchDemo">
-    <Switch></Switch>
+    <Switch :value="y" @update:value="y = $event"></Switch>
+    {{y}}
   </div>
 </template>
 
 <script lang="ts">
 import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
 
 export default {
-  components:{Switch}
+  components:{Switch},
+  setup(){
+    const y = ref(false)
+    return {y}
+  }
 }
 </script>
