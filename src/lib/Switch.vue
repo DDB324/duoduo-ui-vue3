3851bc1cfd53583c57826a236dfb0e4445289496
ddb324 <jiangdaoran@163.com>
2020-12-04 17:10:39 +0800

    点击之后可以切换背景颜色

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,9 +1,19 @@
 <template>
-  <button><span></span></button>
+  <button @click="toggle" :class="{checked}"><span></span></button>
 </template>
 
 <script lang="ts">
-export default {};
+import {ref} from 'vue';
+
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return {checked, toggle};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -12,19 +22,27 @@ $h2: $h - 4px;
 button {
   position: relative;
   height: $h;
-  width:$h*2;
+  width: $h*2;
   border-radius: $h/2;
-  border:none;
+  border: none;
   background: blue;
+
+  &.checked {
+    background: gray;
+
+    span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
 
   span {
     position: absolute;
-    height:$h2;
-    width:$h2;
-    background:white;
+    height: $h2;
+    width: $h2;
+    background: white;
     border-radius: $h2/2;
-    top:2px;
-    left:2px;
+    top: 2px;
+    left: 2px;
   }
 }
 </style>
