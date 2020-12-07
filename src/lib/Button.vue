5ebe72bddda1811aecbb95a48f473600235bf8e2
ddb324 <jiangdaoran@163.com>
2020-12-08 00:38:51 +0800

    支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -17,14 +17,19 @@ export default {
     size: {
       type: String,
       default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal'
     }
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
           return {
             [`duoduo-theme-${theme}`]: theme,
             [`duoduo-size-${size}`]: size,
+            [`duoduo-level-${level}`]: level,
           };
         }
     );
@@ -39,6 +44,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .duoduo-button {
   height: $h;
   border-radius: $radius;
@@ -52,6 +58,7 @@ $radius: 4px;
   justify-content: center;
   align-items: center;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -86,7 +93,7 @@ $radius: 4px;
     color: inherit;
 
     &:hover, &:focus {
-      color: lighten(white, 5%)
+      color: darken(white, 5%)
     }
   }
 
@@ -100,6 +107,58 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+
+  &.duoduo-theme-button {
+    &.duoduo-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover, &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.duoduo-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover, &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.gulu-theme-link {
+    &.gulu-level-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.gulu-theme-text {
+    &.gulu-level-main {
+      color: $blue;
+
+      &:hover, &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.gulu-level-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
