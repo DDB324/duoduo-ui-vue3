90d7bf0f72b39dbc5c075e98315407024b9f6ffb
ddb324 <jiangdaoran@163.com>
2020-12-07 21:45:42 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -52,5 +52,25 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.duoduo-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%)
+    }
+
+    &.duoduo-theme-text {
+      border-color: transparent;
+      box-shadow: none;
+      color: inherit;
+
+      &:hover, &:focus {
+        color: lighten(white, 5%)
+      }
+    }
+  }
 }
 </style>
