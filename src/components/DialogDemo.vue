c6a0b6c4d67cf349d1f027af86cd31a1dbbc84a1
ddb324 <jiangdaoran@163.com>
2020-12-13 00:40:38 +0800

    Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,8 +3,7 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">展示Dialog</Button>
-  <Dialog :visible="x"></Dialog>
-  <div>{{ x }}</div>
+  <Dialog v-model:visible="x" :ok="f1" :cancel="f2" :close-on-click-overlay="false"></Dialog>
 </template>
 
 <script lang="ts">
@@ -22,7 +21,13 @@ export default {
     const toggle = () => {
       x.value = !x.value;
     };
-    return {x, toggle};
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {
+
+    };
+    return {x, toggle,f1,f2};
   }
 };
 </script>
