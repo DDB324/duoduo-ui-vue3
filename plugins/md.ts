e3d8b346152d67216efe15806f28b8152cadb2a9
ddb324 <jiangdaoran@163.com>
2021-01-11 14:11:24 +0800

    优化引入.md的.vue文件

--- plugins/md.ts
+++ plugins/md.ts
@@ -13,9 +13,12 @@ export function md() {
     configureServer: [ // 用于开发
       async ({ app }) => {
         app.use(async (ctx, next) => { // koa
+          //如果路径是以md结尾的
           if (ctx.path.endsWith('.md')) {
+            //把文件变成js文件
             ctx.type = 'js'
             const filePath = path.join(process.cwd(), ctx.path)
+            //文件的内容就是将markdown变成js
             ctx.body = mdToJs(fs.readFileSync(filePath).toString())
           } else {
             await next()
@@ -24,6 +27,7 @@ export function md() {
       },
     ],
     transforms: [{  // 用于 rollup // 插件
+      //将.md结尾的文件的内容变成js
       test: context => context.path.endsWith('.md'),
       transform: ({ code }) => mdToJs(code)
     }]
--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,13 @@ import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
-import Install from './views/Install.vue';
-import GetStarted from './views/GetStarted.vue';
-import Intro from './views/Intro.vue';
+import Markdown from './components/Markdown.vue';
+import Intro from './markdown/Intro.md';
+import Install from './markdown/Install.md';
+import GetStarted from './markdown/GetStarted.md';
+import {h} from 'vue';
+
+const md = string => {return h(Markdown, {content: string, key: string});};
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -18,9 +22,9 @@ export const router = createRouter({
     {
       path: '/doc', component: Doc, children: [
         {path: '', component: DocDemo},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: 'intro', component: md(Intro)},
+        {path: 'get-started', component: md(GetStarted)},
+        {path: 'install', component: md(Install)},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
