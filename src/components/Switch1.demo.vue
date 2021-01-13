833453d41c4861c1615935a2019fd7cc753c5663
ddb324 <jiangdaoran@163.com>
2021-01-13 13:13:57 +0800

    改造Switch Demo

--- src/components/Switch1.demo.vue
+++ src/components/Switch1.demo.vue
@@ -3,23 +3,22 @@
 </demo>
 
 <template>
-  <Switch v-model:value="bool" />
+  <Switch v-model:value="bool"/>
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
-import {
-  ref
-} from 'vue'
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
+
 export default {
   components: {
     Switch,
   },
   setup() {
-    const bool = ref(false)
+    const bool = ref(false);
     return {
       bool
-    }
+    };
   }
-}
+};
 </script>
