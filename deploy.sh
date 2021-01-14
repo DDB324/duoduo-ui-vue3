249f159485b70b7d517d431573166a4521198f69
ddb324 <jiangdaoran@163.com>
2021-01-14 13:14:03 +0800

    使用一键部署

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git branch -M master &&
+git remote add origin git@github.com:DDB324/duoduo-ui-vue3.git
+git push -f -u origin master &&
+cd - &&
+echo https://jiangdaoran.xyz/duoduo-ui-vue3-website/
