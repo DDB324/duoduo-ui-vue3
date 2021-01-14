cf1be448a73d9555afba958f46da3d134fff362a
ddb324 <jiangdaoran@163.com>
2021-01-14 13:19:01 +0800

    修改一键部署提交的仓库地址

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m 'update' &&
 git branch -M master &&
-git remote add origin git@github.com:DDB324/duoduo-ui-vue3.git
+git remote add origin git@github.com:DDB324/duoduo-ui-vue3-website.git
 git push -f -u origin master &&
 cd - &&
 echo https://jiangdaoran.xyz/duoduo-ui-vue3-website/
