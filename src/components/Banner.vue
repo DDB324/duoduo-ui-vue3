c06c3075440f41d3a6d698f6f9b140e7bf98fd67
ddb324 <jiangdaoran@163.com>
2021-01-15 14:36:13 +0800

    封装并使用banner组件

---
+++ src/components/Banner.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="banner">
+    <h1>duoduo-ui</h1>
+    <h2>一个简单的UI框架</h2>
+    <p class="actions">
+      <a href="https://github.com/DDB324/duoduo-ui-vue3">Github</a>
+      <router-link to="/doc">开始</router-link>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.banner {
+  color: $color;
+  padding: 100px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .actions {
+    padding: 8px;
+
+    a {
+      background: $green;
+      color: white;
+      padding: 8px 24px;
+      margin: 0 8px;
+      display: inline-block;
+      border-radius: $border-radius;
+
+      &:hover {
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,25 @@
 <template>
-  <div>
-    <div class="topnavAndBanner">
-      <Topnav/>
-      <div class="banner">
-        <h1>duoduo-ui</h1>
-        <h2>一个简单的UI框架</h2>
-        <p class="actions">
-          <a href="https://github.com/DDB324/duoduo-ui-vue3">Github</a>
-          <router-link to="/doc">开始</router-link>
-        </p>
-      </div>
-    </div>
-    <Features></Features>
+  <div class="topnavAndBanner">
+    <Topnav/>
+    <Banner/>
   </div>
+  <Features/>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
 <script lang="ts">
 import Topnav from '../components/Topnav.vue';
-import Icon from '../components/Icon.vue';
 import Features from '../components/Features.vue';
-
+import Banner from '../components/Banner.vue';
 
 export default {
-  components: {Topnav, Icon, Features}
+  components: {Topnav, Features, Banner}
 };
 </script>
 
 <style lang="scss" scoped>
-$green: #02bcb0;
-$border-radius: 4px;
-$color: #007974;
-.banner {
+.topnavAndBanner {
   background: rgb(227, 255, 240);
   background: linear-gradient(135deg, rgba(227, 255, 240, 1) 0%, rgba(183, 233, 230, 1) 100%);
   clip-path: ellipse(80% 60% at 50% 40%);
-}
-
-.banner {
-  color: $color;
-  padding: 100px 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  .actions {
-    padding: 8px;
-
-    a {
-      background: $green;
-      color: white;
-      padding: 8px 24px;
-      margin: 0 8px;
-      display: inline-block;
-      border-radius: $border-radius;
-
-      &:hover {
-        text-decoration: none;
-      }
-    }
-  }
 }
 </style>
