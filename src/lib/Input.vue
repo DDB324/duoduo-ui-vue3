5e1adba3dc3ed996439e7c4cfb76c9374cca9174
ddb324 <jiangdaoran@163.com>
2021-01-21 14:45:21 +0800

    增加4种输入状态

--- src/lib/Input.vue
+++ src/lib/Input.vue
@@ -1,7 +1,47 @@
 <template>
-  <div class="duoduo-input">
-    <label>
-      <input type="text">
-    </label>
-  </div>
+  <label class="wrapper" :class="{error}">
+    <input class="duoduo-input" type="text" :value="value" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <div class="errorMessage">
+        <d-icon name="prompt-fill"></d-icon>
+        <span>{{ error }}</span>
+      </div>
+    </template>
+  </label>
 </template>
+
+<script lang="ts">
+export default {
+  props: {
+    value: {type: String,},
+    disabled: {type: Boolean, default: false},
+    readonly: {type: Boolean, default: false},
+    error: {type: String,}
+  },
+};
+</script>
+
+<style lang="scss">
+.wrapper {display: inline-flex;align-items: center;
+  font-size: 14px;margin-right: 8px;margin-bottom: 12px;
+
+  :not(:last-child) {margin-right: .5em;}
+
+  .duoduo-input {height: 32px;border: 1px solid #999;
+    border-radius: 4px;padding: 0 8px;font-size: inherit;
+
+    &:focus {box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);outline: none}
+
+    &:hover {border-color: #1a64ea;}
+
+    &[disabled], &[readonly] {border-color: #ccc;color: #ccc;cursor: not-allowed;}
+  }
+
+  &.error {
+    .duoduo-input {border-color: #f1453d;}
+  }
+
+  > .errorMessage {color: #f1453d;vertical-align: bottom;}
+}
+
+</style>
