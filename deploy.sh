12a115f75189500f5b7f7f6b52a75f4abaea7b73
ddb324 <jiangdaoran@163.com>
2021-01-23 23:53:15 +0800

    推送到gitee

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,7 @@ git commit -m 'update' &&
 git branch -M master &&
 git remote add origin git@github.com:DDB324/duoduo-ui-vue3-website.git
 git push -f -u origin master &&
+git remote add gitee git@gitee.com:ddb324/duoduo-ui-vue3-website.git
+git push -f -u gitee master &&
 cd - &&
 echo https://jiangdaoran.xyz/duoduo-ui-vue3-website/
